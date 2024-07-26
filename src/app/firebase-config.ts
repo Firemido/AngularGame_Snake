aaa423a6ff091fad4e594d35c860db4e49a0994e
Mohamed Hazem <73839157+Firemido@users.noreply.github.com>
2024-07-26 03:27:26 +0300

    Update firebase-config.ts

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,9 +1,10 @@
+// add your firebase here for leaderboard score
 export const firebaseConfig = {
-  apiKey: "AIzaSyBX1OaaYURzAA4cOynGay1al9ndlieKl9k",
-  authDomain: "snake-league.firebaseapp.com",
-  projectId: "snake-league",
-  storageBucket: "snake-league.appspot.com",
-  messagingSenderId: "807380276541",
-  appId: "1:807380276541:web:21b0bca24fdc9cc2422154",
-  measurementId: "G-3ST2FX1KTB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
